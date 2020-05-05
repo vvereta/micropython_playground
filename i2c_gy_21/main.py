6ecbcae6a1b375fbdb61f9f1daf8eddc85e957e0
Vitalii Vereta <vitalii.vereta@volvocars.com>
2020-05-05 21:01:53 +0300

    Add SI7021 example

---
+++ i2c_gy_21/main.py
@@ -0,0 +1,58 @@
+import time
+
+from machine import Pin, I2C
+
+
+class SI7021:
+    "I2C Humidity and Temperature Sensor"
+    addr = 0x40
+    command_t = bytearray([0xF3])
+    command_h = bytearray([0xF5])
+
+
+    def __init__(self, *, scl=5,sda=4):
+        self.i2c = I2C(scl=Pin(scl), sda=Pin(sda), freq=100000)
+
+
+    def write(self, command):
+        "Send command to a sensor"
+        self.i2c.writeto(self.addr, self.command_t)
+        time.sleep(0.2)
+
+
+    def read(self):
+        "Read data from a sensor"
+        data = int.from_bytes(self.i2c.readfrom(self.addr, 1), 'big')
+        result = data * 256 + data
+        return result
+
+
+    def get_temperature(self):
+        "Read temperature from a sensor"
+        self.write(self.command_t)
+        data = self.read()
+        cels_temp = (data * 175.72 / 65536.0) - 46.85
+
+        return cels_temp
+
+
+    def get_humidity(self):
+        "Get humidity from a sensor"
+        self.write(self.command_h)
+        data = self.read()
+        humidity = (data * 125 / 65536.0) - 6
+
+        return humidity
+
+
+def main():
+    sensor = SI7021(scl=5,sda=4)
+
+    t = sensor.get_temperature()
+    h = sensor.get_humidity()
+
+    print(t, h)
+
+
+if __name__ == '__main__':
+    main()
